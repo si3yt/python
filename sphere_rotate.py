c4f91e485b6d9c7fab6e3949f18cfe5e1b9de337
ishii <coffeeya3@gmail.com>
2016-10-12 22:52:39 +0900

    rotate commit

--- sphere_rotate.py
+++ sphere_rotate.py
@@ -7,11 +7,11 @@ import matplotlib.image as mpimg
 import os
 
 # 画像の読み込み
-img = cv2.imread("image/sample.jpg", 1)
+img = cv2.imread("image/sample00.jpg", 1)
 height = img.shape[0]
 width = img.shape[1]
 # 結果画像配列作成
-result = np.zeros((height, width, 3), np.uint8)
+result = np.zeros((height * 2, width * 2, 3), np.uint8)
 
 def onTrackbarChanged(x):
     pass
@@ -34,60 +34,62 @@ if __name__ == "__main__":
     cv2.createTrackbar('longitude','sphere_rotate',180,360,onTrackbarChanged)
     cv2.createTrackbar('angle','sphere_rotate',180,360,onTrackbarChanged)
 
+    # get current positions of four trackbars
+    latitude = 90#cv2.getTrackbarPos('latitude','sphere_rotate')
+    longitude = 180#cv2.getTrackbarPos('longitde','sphere_rotate')
+    angle = 180#cv2.getTrackbarPos('angle','sphere_rotate')
+
+    latitude_rad = (latitude - 90) * math.pi / 180      # y軸周り
+    longitude_rad = (longitude - 180) * math.pi / 180   # z軸周り
+    angle_rad = (angle - 180) * math.pi / 180           # z軸周り
+
+    r = height / math.pi
+
+    for h in range(height):
+        for w in range(width):
+            # 円筒平面 → 球(極座標)
+            sphere_lat_rad = h / r
+            sphere_lon_rad = w / r
+
+            x = r * math.sin(sphere_lat_rad) * math.cos(sphere_lon_rad)
+            y = r * math.sin(sphere_lat_rad) * math.sin(sphere_lon_rad)
+            z = r * math.cos(sphere_lat_rad)
+
+            # z軸周り
+            z_rotate = np.matrix([[math.cos(longitude_rad),math.sin(longitude_rad),0],[- math.sin(longitude_rad), math.cos(longitude_rad),0],[0,0,1]])
+            # y軸周り
+            y_rotate = np.matrix([[math.cos(latitude_rad),0,- math.sin(latitude_rad)],[0,1,0],[math.sin(latitude_rad),0,math.cos(latitude_rad)]])
+            # x軸周り
+            x_rotate = np.matrix([[1,0,0],[0,math.cos(angle_rad),math.sin(angle_rad)],[0,- math.sin(angle_rad),math.cos(angle_rad)]])
+            # 行列の積
+            matrix = z_rotate.dot(y_rotate)
+            matrix = matrix.dot(x_rotate)
+
+            new_x = x * matrix[0,0] + y * matrix[1,0] + z * matrix[2,0]
+            new_y = x * matrix[0,1] + y * matrix[1,1] + z * matrix[2,1]
+            new_z = x * matrix[0,2] + y * matrix[1,2] + z * matrix[2,2]
+
+            distance = math.sqrt(new_x**2 + new_y**2 + new_z**2)
+            theta = math.acos(new_z/distance) # 値域:0~pi # 緯度
+            phi = math.atan2(new_y,new_x) # 値域:-pi~pi # 経度
+
+            if phi < 0:
+                phi = 2 * math.pi + phi
+
+            ## 円筒展開
+            cylinder_x = int(phi * r) # 経度 * 球体半径
+            cylinder_y = int(theta * r) # 経度 * 球体半径
+
+            result[cylinder_y][cylinder_x][0] = img[h][w][0]
+            result[cylinder_y][cylinder_x][1] = img[h][w][1]
+            result[cylinder_y][cylinder_x][2] = img[h][w][2]
+
+
 
     while(1):
         cv2.imshow("sphere_rotate", result)
         k = cv2.waitKey(1)
         if k == 27: # ESCキーで終了
             break
-        # get current positions of four trackbars
-        latitude = cv2.getTrackbarPos('latitude','sphere_rotate')
-        longitude = cv2.getTrackbarPos('longitude','sphere_rotate')
-        angle = cv2.getTrackbarPos('angle','sphere_rotate')
-
-        latitude_rad = (latitude - 90) * math.pi / 180      # x軸周り
-        longitude_rad = (longitude - 180) * math.pi / 180   # z軸周り
-        angle_rad = (angle - 180) * math.pi / 180           # y軸周り
-
-        r = height / math.pi
-
-        for h in range(height):
-            for w in range(width):
-                # 円筒平面 → 球(極座標)
-                sphere_lat_rad = h / r
-                sphere_lon_rad = w / r
-
-                x = r * math.sin(sphere_lat_rad) * math.cos(sphere_lon_rad)
-                y = r * math.sin(sphere_lat_rad) * math.sin(sphere_lon_rad)
-                z = r * math.cos(sphere_lat_rad)
-
-                # z軸周り
-                z_rotate = np.matrix([[math.cos(longitude_rad),math.sin(longitude_rad),0],[- math.sin(longitude_rad), math.cos(longitude_rad),0],[0,0,1]])
-                # y軸周り
-                y_rotate = np.matrix([[math.cos(angle_rad),0,- math.sin(angle_rad)],[0,1,0],[math.sin(angle_rad),0,math.cos(angle_rad)]])
-                # z軸周り
-                x_rotate = np.matrix([[1,0,0],[0,math.cos(latitude_rad),math.sin(latitude_rad)],[0,- math.sin(latitude_rad),math.cos(latitude_rad)]])
-                # 行列の積
-                matrix = z_rotate.dot(y_rotate)
-                matrix = matrix.dot(x_rotate)
-
-                new_x = x * (matrix[0,0] + matrix[1,0] + matrix[2,0])
-                new_y = y * (matrix[0,1] + matrix[1,1] + matrix[2,1])
-                new_z = z * (matrix[0,2] + matrix[1,2] + matrix[2,2])
-
-                distance = math.sqrt(new_x**2 + new_y**2 + new_z**2)
-                theta = math.acos(new_z/distance) # 値域:0~pi # 緯度
-                phi = math.atan2(new_y,new_x) # 値域:-pi~pi # 経度
-
-                if phi < 0:
-                    phi = 2 * math.pi + phi
-
-                ## 円筒展開
-                cylinder_x = int(phi * r) # 経度 * 球体半径
-                cylinder_y = int(theta * r) # 経度 * 球体半径
-
-                result[cylinder_y][cylinder_x][0] = img[h][w][0]
-                result[cylinder_y][cylinder_x][1] = img[h][w][1]
-                result[cylinder_y][cylinder_x][2] = img[h][w][2]
 
     cv2.destroyAllWindows()
