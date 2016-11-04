4a25e00608fc7bd954ef0dff404bdc2d76b5c77f
ishii <coffeeya3@gmail.com>
2016-11-04 22:42:03 +0900

    commit

--- HoughLine/hough_normal.py
+++ HoughLine/hough_normal.py
@@ -7,11 +7,20 @@ edges = cv2.Canny(gray,50,150,apertureSize = 3)
 minLineLength = 100
 maxLineGap = 10
 
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+lines = cv2.HoughLines(edges,1,np.pi/180,200)
 
 for i in range(0,len(lines)-1):
-    for x1,y1,x2,y2 in lines[i]:
-        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+    for rho,theta in lines[i]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
 while(1):
     cv2.imshow("sphere_rotate", edges)
